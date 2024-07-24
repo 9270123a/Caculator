6709304e8460f4d428eba0c20920d331ef8035f6
9270123a <9270123a@gmail.com>
2024-07-24 20:29:44 +0800

    新版本 EVENTHANDLE

---
+++ Calcuatoe-new/小算盤/EventHandlers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 小算盤
+{
+    internal class EventHandlers
+    {
+        public static event EventHandler<double> ResultEvent;
+
+        public static void NotifyEvent(double number)
+        {
+            ResultEvent.Invoke(null, number);
+        }
+
+    }
+}
---
+++ Calcuatoe-new/小算盤/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 小算盤
+{
+    public partial class Form1 : Form
+    {
+
+        string CurrentOperation;
+        string number1;
+        string number2;
+        bool newinput = true;
+        public Form1()
+        {
+            InitializeComponent();
+            EventHandlers.ResultEvent += EventHandlers_ResultEvent;
+
+
+    }
+
+    private void EventHandlers_ResultEvent(object sender, double e)
+        {
+            ResultTxt.Text = "";
+            ResultTxt.Text = e.ToString();
+            number1 = e.ToString();
+            number2 = "";
+            newinput = true;
+        }
+
+        private void NumberClick(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+
+            if (!string.IsNullOrEmpty(CurrentOperation))
+            {
+                if (newinput)
+                {
+                    ResultTxt.Text = "";
+                    newinput = false;
+                }
+                number2 += button.Text;
+                ResultTxt.Text += button.Text;
+            }
+            else
+            {
+                number1 += button.Text;
+                ResultTxt.Text += button.Text;
+            }
+        }
+    
+        private void OperClick(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+
+            if (!string.IsNullOrEmpty(number2))
+            {
+                Controller controller = new Controller();
+                controller.Calculator(number1, number2, CurrentOperation);
+            }
+            CurrentOperation = button.Text;
+
+        }
+
+        private void EqualsClick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(number2))
+            {
+                Controller controller = new Controller();
+                controller.Calculator(number1, number2, CurrentOperation);
+            }
+
+        }
+
+
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+    }
+}
